eec3e0d6912ad5455da8698b387fd8076bd2ccdf
Uyen Truong <haiuyentruong@gmail.com>
2019-02-09 22:14:16 +0200

    Add findutils script

---
+++ ch5/findutils-fixes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' gl/lib/*.c
+sed -i '/unistd/a #include <sys/sysmacros.h>' gl/lib/mountlist.c
+echo "#define _IO_IN_BACKUP 0x100" >> gl/lib/stdio-impl.h
