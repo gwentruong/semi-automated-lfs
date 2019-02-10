8c11a98e3a0c24bce2f3f51fd4a132adf0fc1de7
Uyen Truong <haiuyentruong@gmail.com>
2019-02-10 21:40:08 +0200

    Add perl script

---
+++ ch6/perl-configure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export BUILD_ZLIB=False
+export BUILD_BZIP2=0
+
+sh Configure -des -Dprefix=/usr \
+             -Dvendorprefix=/usr \
+             -Dman1dir=/usr/share/man/man1 \
+             -Dman3dir=/usr/share/man/man3 \
+             -Dpager="/usr/bin/less -isR" \
+             -Duseshrplib \
+             -Dusethreads
