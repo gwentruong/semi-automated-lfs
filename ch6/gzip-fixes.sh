b548b4935ac3eae4a8c52f2576d16b5607e07e1d
Uyen Truong <haiuyentruong@gmail.com>
2019-02-10 22:18:46 +0200

    Add gzip scripts

---
+++ ch6/gzip-fixes.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
+echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.hsed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
+echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
---
+++ ch6/gzip-mv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mv -v /usr/bin/gzip /bin
