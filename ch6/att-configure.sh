db83011538098b5a20e85b3bdaa940f1b9f98e5a
Uyen Truong <haiuyentruong@gmail.com>
2019-02-10 21:27:37 +0200

    Add att scripts

---
+++ ch6/att-configure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./configure --prefix=/usr \
+            --bindir=/bin \
+            --disable-static \
+            --sysconfdir=/etc \
+            --docdir=/usr/share/doc/attr-2.4.48
