ccccd0993591db00f142067750c581e8485d63a3
Uyen Truong <haiuyentruong@gmail.com>
2019-02-10 21:27:19 +0200

    Add acl scripts

---
+++ ch6/acl-configure.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+./configure --prefix=/usr \
+            --bindir=/bin \
+            --disable-static \
+            --libexecdir=/usr/lib \
+            --docdir=/usr/share/doc/acl-2.2.53
