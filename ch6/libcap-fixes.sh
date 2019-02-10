76f6c20aea5f6e68db02476ca356d792ad42cdbe
Uyen Truong <haiuyentruong@gmail.com>
2019-02-10 21:29:57 +0200

    Add libcap scripts

---
+++ ch6/libcap-fixes.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sed -i '/install.*STALIBNAME/d' libcap/Makefile
---
+++ ch6/libcap-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+make RAISE_SETFCAP=no lib=lib prefix=/usr install
+chmod -v 755 /usr/lib/libcap.so
+
+mv -v /usr/lib/libcap.so.* /lib
+ln -sfv ../../lib/$(readlink /usr/lib/libcap.so) /usr/lib/libcap.so
