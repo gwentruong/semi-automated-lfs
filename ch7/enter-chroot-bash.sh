e6e7f767f2b0718d9bc66977bee7916d82cf6768
Uyen Truong <haiuyentruong@gmail.com>
2019-02-12 21:06:10 +0200

    Updated enter chroot env method

---
+++ ch7/enter-chroot-bash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+chroot "$LFS" /usr/bin/env -i \
+        HOME=/root TERM="$TERM" \
+        PS1='(lfs chroot) \u:\w\$ ' \
+        PATH=/bin:/usr/bin:/sbin:/usr/sbin \
+        /bin/bash --login
